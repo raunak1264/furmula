86e0c4ca64bb57d5e5e4739c4d4dcb3bd30a63ab
Raunak <rawnakkapoor@gmail.com>
2021-05-31 18:33:26 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        double v=2,u=3,a=2,s=1;
+        System.out.println((v*v-u*u)/(2*a*s));//2*2-3*3/2*2*1=-6/4
+    }
+}
